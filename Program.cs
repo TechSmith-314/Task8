7d7af23ac8373eed77996c7c46af4fac9e269f76
Евгений Пустовалов <techsmith314@hotmail.com>
2023-01-25 11:47:28 +0800

    Исправлено именование константной переменной

--- Program.cs
+++ Program.cs
@@ -6,18 +6,18 @@ namespace Task8
     {
         static void Main(string[] args)
         {
+            const int MinutesInHour = 60;
             int queueLength;
             int timePassageOnePatient = 10;
             int numberOfWaitingHours;
             int numberOfWaitingFullMinutes;
-            int numberOfWaitingMinutes;
-            const int minutesInHour = 60;
+            int numberOfWaitingMinutes;            
 
             Console.WriteLine("Сколько пациентов в очереди перед Вами?");
             queueLength = Int32.Parse(Console.ReadLine());
             numberOfWaitingFullMinutes = queueLength * timePassageOnePatient;
-            numberOfWaitingHours = numberOfWaitingFullMinutes / minutesInHour;
-            numberOfWaitingMinutes = numberOfWaitingFullMinutes % minutesInHour;
+            numberOfWaitingHours = numberOfWaitingFullMinutes / MinutesInHour;
+            numberOfWaitingMinutes = numberOfWaitingFullMinutes % MinutesInHour;
 
             Console.WriteLine($"Вам осталось ждать {numberOfWaitingHours} час(а/ов), {numberOfWaitingMinutes} минут");
         }
